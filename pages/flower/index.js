377545f377c13cd83ba850f74012463e41638771
zhaohongze <zhaohongze@kuaishou.com>
2024-07-01 16:54:11 +0800

    fixed cached key

--- pages/flower/index.js
+++ pages/flower/index.js
@@ -153,7 +153,7 @@ Page({
     getTodayRandsKey() {
         const now = new Date();
         const today = `${now.getFullYear()}${now.getMonth()+1}${now.getDate()}`;
-        return `yuanbao.today.rands.${today}`;
+        return `flower.today.rands.${today}`;
     },
 
     /**
